54e44d70750c69caa620ab653c291b31a0060289
maksymmaksym029 <dsw58027@student.dsw.edu.pl>
2025-06-04 20:13:13 +0200

    Update installResources.ps1

--- installResources.ps1
+++ installResources.ps1
@@ -1,4 +1,14 @@
-pip install pyinstaller
-pip install pyyaml
-pip install lxml
-pip install pyqt5
+# installResources.ps1
+# Instalacja wymaganych bibliotek
+
+# Upewnij się, że pip jest dostępny
+python -m ensurepip --upgrade
+python -m pip install --upgrade pip
+
+# Instalacja z pliku requirements.txt
+pip install -r requirements.txt
+
+# Dodatkowe biblioteki (jeśli nie są w requirements.txt)
+pip install lxml         # Obsługa XML (jeśli nie używasz xmltodict)
+pip install xmltodict    # Alternatywnie do lxml
+pip install jsonschema   # Jeśli walidujesz JSON
